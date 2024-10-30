309e92a28e3df430d75a14192b5d0c383cdac666
Adota2107 <nhatminh21072003@gmail.com>
2024-10-30 16:35:44 +0700

    create class_dialog.xml, ic_add_black_24dp.xml. Update activity_main.xml, MainActivity.java

--- app/src/main/java/com/example/prm_project/MainActivity.java
+++ app/src/main/java/com/example/prm_project/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.prm_project;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fabMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        fabMain = findViewById(R.id.fab_main);
+        fabMain.setOnClickListener(v -> showDialog());
+    }
+
+    // show dialog to add new class
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = LayoutInflater.from(this).inflate(R.layout.class_dialog, null);
+
+        builder.setView(view);
+        builder.create().show();
+
     }
 }
